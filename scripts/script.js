bacb531e08d43032203b4e3b6fd16fbad941d357
Cristian Camargo <ccamargo@seismic.com>
2019-06-06 15:33:03 -0700

    Implement color switcher and title switcher

--- scripts/script.js
+++ scripts/script.js
@@ -11,10 +11,38 @@ var linkMappings = {
     "portfolioComponent": "portfolioLink"
 }
 
+var currentProject = "treacherousAdventure"
+
 var projects = [
     "treacherousAdventure",
     "smartParkDisney",
     "vrcane"
+]
+
+var colorSchemes = {
+    "default": {
+        "primary": "#BA5B50",
+        "secondary": "#F0E5DD",
+        "tertiary": "#D1CEBE"
+    },
+    "blue": {
+        "primary": "#435892",
+        "secondary": "#F0E5DD",
+        "tertiary": "#BCAA8A"
+    },
+    "green": {
+        "primary": "#8DA077",
+        "secondary": "#F0E5DD",
+        "tertiary": "#BABAAB"
+    }
+}
+
+var titles = [
+    "Software Engineer",
+    "Data Engineer",
+    "Full Stack Engineer",
+    "Web Engineer",
+    "Mobile Engineer"
 ]
 
 function ChangeToComponent(activeId) {
@@ -42,8 +70,6 @@ function ChangeToComponent(activeId) {
     })
 }
 
-var currentProject = "treacherousAdventure"
-
 function NextProject() {
     // Get the index of the next project
     var nextProjectIndex = projects.indexOf(currentProject) + 1
@@ -66,4 +92,32 @@ function NextProject() {
 
     // Update current project
     currentProject = nextProjectId
+}
+
+function OpenCloseColorSchemas() {
+    container = document.getElementsByClassName('colorSwitchContainer').item(0)
+    display = container.style.display
+    if (display == 'flex') {
+        document.getElementsByClassName('colorSwitchContainer').item(0).style.display = "none"
+    }
+    else {
+        document.getElementsByClassName('colorSwitchContainer').item(0).style.display = "flex"
+    }
+}
+
+function ChangeColorSchema(schema) {
+    // Change color vars
+    document.documentElement.style.setProperty('--primary-color', colorSchemes[schema]["primary"]);
+    document.documentElement.style.setProperty('--secondary-color', colorSchemes[schema]["secondary"]);
+    document.documentElement.style.setProperty('--tertiary-color', colorSchemes[schema]["tertiary"]);
+
+    // Close color schemas
+    document.getElementsByClassName('colorSwitchContainer').item(0).style.display = "none"
+}
+
+function CycleTitle() {
+    title = titles[0]
+    titles = titles.slice(1, titles.length)
+    titles.push(title)
+    document.getElementById("title").textContent = title
 }
