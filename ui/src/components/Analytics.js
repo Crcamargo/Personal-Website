608503896d017ffabcfbe1e8526b445c84bf7a36
Cristian Camargo <ccamargo@ucsd.edu>
2021-01-02 12:49:29 -0800

    Add firebase analytics to ui

--- ui/src/components/Analytics.js
+++ ui/src/components/Analytics.js
@@ -1,162 +1,131 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { PieChart, Pie, Cell, Tooltip } from 'recharts'
 import { connect } from 'react-redux'
+import { firebase } from '@firebase/app'
 
+require('firebase/firestore')
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyBzcYlzBvTZVyJw5eib7aLSNGYXpqlAU5g',
+  authDomain: 'cristian-camargo.firebaseapp.com',
+  projectId: 'cristian-camargo'
+});
+
+const db = firebase.firestore()
 const maxNavRows = 10
-
-const synthData = {
-  totalWebViews: 3574,
-  totalResumeDownloads: 535,
-  browserDistribution: [
-    {
-      name: 'Chrome',
-      value: 890
-    },
-    {
-      name: 'FireFox',
-      value: 743
-    },
-    {
-      name: 'Other',
-      value: 632
-    }
-  ],
-  totalNavigationClicks: [
-    {
-      name: 'Home',
-      value: 123
-    },
-    {
-      name: 'Experience',
-      value: 1235
-    },
-    {
-      name: 'Projects',
-      value: 4321
-    },
-    {
-      name: 'Analytics',
-      value: 2312
-    }
-  ],
-  totalContactClicks: [
-    {
-      name: 'Email',
-      value: 123
-    },
-    {
-      name: 'Phone',
-      value: 1235
-    },
-    {
-      name: 'LinkedIn',
-      value: 4321
-    },
-    {
-      name: 'Twitter',
-      value: 2312
-    }
-  ]
-}
-
-const COLORS = ['#757575', '#BA5B50', '#435892', '#546E7A'];
+const shardsDocumentId = 'FhstJMJq47MCjYVAZVOc'
+const COLORS = ['#BA5B50', '#415a77', '#546E7A', '#757575'];
 const renderLabel = entry => entry.name
 
-const Analytics = ({ data, navigationClicks }) => (
-  <div className="analytics-container">
-    <div className="analytics-metric-container">
-      <span className="analytics-metric-title">Total Visits</span>
-      <span className="analytics-metric">{data.totalWebViews}</span>
+function getDistribution(data, dataKey) {
+  let dist = {}
+
+  data.forEach(d => {
+    let name = d.browser[dataKey]
+
+    if (!dist[name]) {
+      dist[name] = {
+        name,
+        value: 1
+      }
+    }
+    else {
+      dist[name].value += 1
+    }
+  })
+
+  return Object.keys(dist).map(k => dist[k])
+}
+
+const Analytics = ({ navigationClicks }) => {
+  const [totalWebViews, setTotalWebView] = useState(0)
+  const [totalResumeDownloads, setTotalResumeDownloads] = useState(0)
+  const [browserDistribution, setBrowserDist] = useState([])
+  const [osDistribution, setOsDist] = useState([])
+
+  useEffect(() => {
+    db.collection('analytics').doc(shardsDocumentId).collection('shards').onSnapshot(c => {
+      let totalWebViews = 0
+      let totalResumeDownloads = 0
+
+      c.docs.forEach(doc => {
+        let data = doc.data()
+        totalWebViews += data.totalViews
+        totalResumeDownloads += data.totalResumeDownloads
+      })
+
+      setTotalWebView(totalWebViews)
+      setTotalResumeDownloads(totalResumeDownloads)
+    })
+
+    db.collection('events').doc('raw').collection('views').onSnapshot(c => {
+      let data = c.docs.map(doc => doc.data())
+
+      setBrowserDist(getDistribution(data, 'name'))
+      setOsDist(getDistribution(data, 'os'))
+    })
+  }, [])
+
+  return(
+    <div className="analytics-container">
+      <div className="analytics-metric-container">
+        <span className="analytics-metric-title">Total Visits</span>
+        <span className="analytics-metric">{totalWebViews}</span>
+      </div>
+      <div className="analytics-metric-container">
+        <span className="analytics-metric-title">Total Resume Downloads</span>
+        <span className="analytics-metric">{totalResumeDownloads}</span>
+      </div>
+      <div className="analytics-metric-container">
+        <span className="analytics-metric-title">Browser Distribution</span>
+        <PieChart width={500} height={200}>
+          <Tooltip />
+          <Pie data={browserDistribution} dataKey='value' label={renderLabel}>
+            {browserDistribution.map((val, i) => <Cell key={i} fill={COLORS[i % COLORS.length]}/>)}
+          </Pie>
+        </PieChart>
+      </div>
+      <div className="analytics-metric-container">
+        <span className="analytics-metric-title">OS Distribution</span>
+        <PieChart width={500} height={200}>
+          <Tooltip />
+          <Pie data={osDistribution} dataKey='value' label={renderLabel}>
+            {osDistribution.map((val, i) => <Cell key={i} fill={COLORS[i % COLORS.length]}/>)}
+          </Pie>
+        </PieChart>
+      </div>
+      <div className="analytics-metric-container">
+        <span className="analytics-metric-title">Your Page Clicks</span>
+        <table>
+          <tr>
+            <th>Button Clicked</th>
+            <th>Occured At</th>
+          </tr>
+          <tbody>
+          {
+            navigationClicks.map((val, i) => {
+              return (
+                <tr key={i}>
+                  <td>{val.itemClicked}</td>
+                  <td>{val.occurredAt.toString()}</td>
+                </tr>
+              )
+            })
+          }
+          </tbody>
+        </table>
+      </div>
     </div>
-    <div className="analytics-metric-container">
-      <span className="analytics-metric-title">Total Resume Downloads</span>
-      <span className="analytics-metric">{data.totalResumeDownloads}</span>
-    </div>
-    <div className="analytics-metric-container">
-      <span className="analytics-metric-title">Total Navigation Clicks</span>
-      <PieChart width={500} height={200}>
-        <Tooltip />
-        <Pie data={data.totalNavigationClicks} dataKey='value' label={false} >
-          {data.totalNavigationClicks.map((val, i) => <Cell key={i} fill={COLORS[i % COLORS.length]}/>)}
-        </Pie>
-      </PieChart>
-    </div>
-    <div className="analytics-metric-container">
-      <span className="analytics-metric-title">Total Contact Clicks</span>
-      <PieChart width={500} height={200}>
-        <Tooltip />
-        <Pie data={data.totalContactClicks} dataKey='value' label={false}>
-          {data.totalContactClicks.map((val, i) => <Cell key={i} fill={COLORS[i % COLORS.length]}/>)}
-        </Pie>
-      </PieChart>
-    </div>
-    <div className="analytics-metric-container">
-      <span className="analytics-metric-title">Browser Distribution</span>
-      <PieChart width={500} height={200}>
-        <Tooltip />
-        <Pie data={data.browserDistribution} dataKey='value' label={false}>
-          {data.browserDistribution.map((val, i) => <Cell key={i} fill={COLORS[i % COLORS.length]}/>)}
-        </Pie>
-      </PieChart>
-    </div>
-    <div className="analytics-metric-container">
-      <span className="analytics-metric-title">Your Page Clicks</span>
-      <table>
-        <tr>
-          <th>Button Clicked</th>
-          <th>Occured At</th>
-        </tr>
-        <tbody>
-        {
-          navigationClicks.map((val, i) => {
-            return (
-              <tr key={i}>
-                <td>{val.itemClicked}</td>
-                <td>{val.occurredAt.toString()}</td>
-              </tr>
-            )
-          })
-        }
-        </tbody>
-      </table>
-    </div>
-  </div>
-)
+)}
 
 const mapStateToProps = (state) => {
-  // const greetingClicks = state.events.navItemClicks.filter((e, i) => e.item === "greeting")
-  // const experienceClicks = state.events.navItemClicks.filter((e, i) => e.item === "experience")
-  // const projectsClicks = state.events.navItemClicks.filter((e, i) => e.item === "projects")
-  // const analyticsClicks = state.events.navItemClicks.filter((e, i) => e.item === "analytics")
-
   let end = state.events.clicks.length
   let start = Math.max(0, end - maxNavRows)
 
   const navigationClicks = state.events.clicks.slice(start, end)
-  
-  // const sessionNavClicks = [
-  //   {
-  //     name: 'Home Page Clicks',
-  //     value: greetingClicks.length
-  //   },
-  //   {
-  //     name: 'Experience Page Clicks',
-  //     value: experienceClicks.length
-  //   },
-  //   {
-  //     name: 'Projects Page Clicks',
-  //     value: projectsClicks.length
-  //   },
-  //   {
-  //     name: 'Analytics Page Clicks',
-  //     value: analyticsClicks.length
-  //   }
-  // ]
 
   return {
-    data: synthData,
     navigationClicks,
-    //sessionNavClicks
   }
 }
 
--- ui/src/components/Main.js
+++ ui/src/components/Main.js
@@ -1,36 +1,11 @@
 import React from 'react'
-import { connect } from 'react-redux'
 import Home from './Home'
 import Experience from './Experience'
 import Projects from './Projects'
 import Analytics from './Analytics'
-import Constants from '../Constants'
 import { Switch, Route } from   'react-router-dom'
 
-const Main = ({ tab }) => {
-  let comp = undefined
-
-  switch (tab) {
-    case Constants.State.Tabs.Greeting:
-      comp = <Home />
-      break
-    
-    case Constants.State.Tabs.Experience:
-      comp = <Experience />
-      break
-    
-    case Constants.State.Tabs.Projects:
-      comp = <Projects />
-      break
-    
-    case Constants.State.Tabs.Analytics:
-      comp = <Analytics />
-      break
-    
-    default:
-      comp = <span>{tab}</span>
-  }
-
+export default () => {
   return(
     <div className='main-container'>
       <Switch>
@@ -40,6 +15,9 @@ const Main = ({ tab }) => {
         <Route path="/projects">
           <Projects />
         </Route>
+        <Route path="/analytics">
+          <Analytics />
+        </Route>
         <Route path="/">
           <Home />
         </Route>
@@ -47,11 +25,3 @@ const Main = ({ tab }) => {
     </div>
   )
 }
-
-const mapStateToProps = (state) => {
-  return {
-    tab: state.current.tab
-  }
-}
-
-export default connect(mapStateToProps)(Main)
