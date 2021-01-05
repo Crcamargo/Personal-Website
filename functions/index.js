fe439efe777a73677419ee30216b3bd6cdd1e7f6
Cristian Camargo <ccamargo@ucsd.edu>
2021-01-04 20:31:19 -0800

    Redirect all non api calls to ui

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 const functions = require('firebase-functions');
+const path = require('path');
 
 var routes = require('./app/routes')
 var express = require('express');
@@ -8,14 +9,16 @@ var cors = require('cors')
 /* Set up express app */
 var app = express().use(bodyParser.json())
                    .use(cors())
-                   .use(express.static('../ui/build'))
+                   .use(express.static('./build'))
 
 // TODO add rate limiting middleware
 
-/* Set up routes */
+/* Set up api routes */
 var router = express.Router();
 router.post('/events', routes.events);
-
 app.use('/api', router);
+
+/* Resolve any other calls as the web app */
+app.get('*', (req, res) => res.sendFile(path.resolve('.', 'build', 'index.html')))
 
 exports.app = functions.https.onRequest(app)
